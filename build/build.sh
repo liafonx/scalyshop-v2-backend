615c95bd1be69db48241a276f93c0ae6310e1aa9
Liafonx <liafonx@outlook.com>
2025-02-14 00:17:26 +0100

    update cache

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 # Optional: trap errors to print the line number where the error occurred.
 trap 'echo "Error occurred at line $LINENO. Exiting..."; exit 1' ERR
 
-docker build -t $CI_APPLICATION_REPOSITORY .
+docker pull $CI_APPLICATION_REPOSITORY || true
+docker build --cache-from $CI_APPLICATION_REPOSITORY -t $CI_APPLICATION_REPOSITORY .
 docker tag $CI_APPLICATION_REPOSITORY $CI_APPLICATION_TAG
 
 docker push $CI_APPLICATION_REPOSITORY
